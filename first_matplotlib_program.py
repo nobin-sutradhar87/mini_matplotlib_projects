b7b4f0c326159ad36c664c3e3333a81b19a6136c
phantom0 <official.nobinsutradhar@gmail.com>
2025-09-04 09:26:19 +0530

    modified first_matplotlib.. added linewidth,set_title, x and y_label, and tick_params

--- first_matplotlib_program.py
+++ first_matplotlib_program.py
@@ -6,5 +6,11 @@ num = [1, 2, 3, 4, 5]
 num_squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(num, num_squares)
+ax.plot(num, num_squares, linewidth=3)
+ax.set_title("square number", fontsize=24)
+
+ax.set_xlabel("values", fontsize=24)
+ax.set_ylabel("squared number",fontsize=24)
+
+ax.tick_params(axis='both', labelsize=14)
 plt.show()
